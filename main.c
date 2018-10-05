a80137e26a87206685a810d66c4b5ccc0c50b6e9
TMC94 <tc818912@ohio.edu>
2018-10-04 21:58:06 -0400

    first commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i = 0;
+
+    while(i <= 100)
+    {
+        if(i % 2 == 1)
+        {
+            printf("%d\n", i);
+        }
+        i++;
+    }
+    return 0;
+}
